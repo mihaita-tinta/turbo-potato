971a57afda17e3f37616889d27866bad0a9ad28f
Mihaita Tinta <mihaita.tinta@ing.com>
2021-05-26 17:21:06 +0300

    init

---
+++ src/main/java/com/turbo/potato/turbopotato/TurboPotatoApplication.java
@@ -0,0 +1,13 @@
+package com.turbo.potato.turbopotato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurboPotatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurboPotatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turbo/potato/turbopotato/TurboPotatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turbo.potato.turbopotato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurboPotatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
